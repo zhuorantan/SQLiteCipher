be4078be3861c05f1a3a12abca0ef8120dff18d6
GitHub <noreply@github.com>
2023-06-02 15:22:44 +0000

    Upgrade to 0.14.1

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     .binaryTarget(
       name: "SQLite",
       url: "https://github.com/zhuorantan/SQLiteCipher/releases/download/0.14.1/SQLite.xcframework.zip",
-      checksum: "1ad444ffe3f05f86941590a204394f6206c9de4b5e611bc407385886289d7bdd"
+      checksum: "8cb61cc74e3d04cc26f9bb3a693929ba1f047d378f88cbe2e4f27023b6c12475"
     ),
 
     .binaryTarget(
       name: "SQLCipher",
       url: "https://github.com/zhuorantan/SQLiteCipher/releases/download/0.14.1/SQLCipher.xcframework.zip",
-      checksum: "7d5128d6a89346fc40fa3c9f7854900a16b8db49b05e8f48ffebb0c1b38ab7cc"
+      checksum: "b8e62b4104ba8b1b4f6496b0fa5c3bd76ecf710e461b65ebcec74532788a2446"
     ),
   ]
 )
